a1c29b46bfc00a158af8878f7b059c8cecbcc508
hnasser-dev <hnasser.dev@proton.me>
2026-01-06 23:20:30 +0000

    optimalNextGuess sorted. trying to filter the word list on each guess

--- cmd/play/main.go
+++ cmd/play/main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"log"
+	"math"
 	"reflect"
+	"sort"
 
 	"github.com/messy-coding/wordle-solver/internal/words"
 )
 
 const wordLength int = 5
+
 const maxNumGuesses int = 6
 
 type Colour uint8
@@ -18,29 +21,12 @@ const (
 	Green
 )
 
-var correctGuessColourPattern = [wordLength]Colour{Green, Green, Green, Green, Green}
-
-func getOptimalNextGuess(remainingWords []string) string {
-	/*
-		Iterate through all remaining words
-		For each word, calculate the colour distribution pattern across all other words
-		...
-	*/
-
-	// bitsPerGuess := map[string]float64{}
-
-	for _, potentialGuess := range remainingWords {
-		colourPatternNums := map[[5]Colour]int{}
-		for _, potentialAnswer := range remainingWords {
-			colourPattern := getColourPattern(potentialGuess, potentialAnswer)
-			colourPatternNums[colourPattern]++
-		}
-		// TODO - calculate numBitsOfInfo (weighted sum of probabilities, then convert to bits using -log(p))
-		// bitsPerGuess[potentialGuess] = numBitsOfInfo
-	}
-
-	return "hello"
+type guessEntropy struct {
+	Guess string
+	Bits  float64
 }
+
+var correctGuessColourPattern = [wordLength]Colour{Green, Green, Green, Green, Green}
 
 func getColourPattern(guess string, answer string) [wordLength]Colour {
 
@@ -73,10 +59,56 @@ func getColourPattern(guess string, answer string) [wordLength]Colour {
 		}
 	}
 
-	log.Println(colourPatternSlice)
-	log.Println(numCharsGuessed)
-
 	return colourPatternSlice
+}
+
+// TODO - fix this so it works - currently gets stuck - and not sure if this even makes sense anyway
+func filterWordListBasedOnLatestGuess(wordList []string, guessColourPattern [wordLength]Colour, answer string) []string {
+	filteredWordList := []string{}
+	for _, word := range wordList {
+		wordCouldBeAnswer := reflect.DeepEqual(guessColourPattern, getColourPattern(word, answer))
+		if wordCouldBeAnswer {
+			filteredWordList = append(filteredWordList, word)
+		}
+	}
+	return filteredWordList
+}
+
+func getOptimalNextGuess(remainingWords []string) string {
+
+	entropies := map[string]float64{}
+
+	for _, potentialGuess := range remainingWords {
+
+		colourPatternNums := map[[wordLength]Colour]int{}
+
+		for _, potentialAnswer := range remainingWords {
+			colourPattern := getColourPattern(potentialGuess, potentialAnswer)
+			colourPatternNums[colourPattern]++
+		}
+
+		numRemainingWords := len(remainingWords)
+		numBitsOfInfo := 0.0
+
+		for _, numWordsInPath := range colourPatternNums {
+			probability := float64(numWordsInPath) / float64(numRemainingWords)
+			numBitsOfInfo += float64(probability) * math.Log2(1.0/probability)
+		}
+		entropies[potentialGuess] = numBitsOfInfo
+	}
+
+	pairs := make([]guessEntropy, 0, len(entropies))
+
+	for guess, bits := range entropies {
+		pairs = append(pairs, guessEntropy{guess, bits})
+	}
+
+	sort.Slice(pairs, func(i, j int) bool { return pairs[i].Bits > pairs[j].Bits })
+
+	// TODO - remove
+	log.Printf("Top 3 pairs: %v", pairs[:3])
+
+	return pairs[0].Guess
 }
 
 func playGame(answer string, wordList []string) error {
@@ -89,13 +121,15 @@ func playGame(answer string, wordList []string) error {
 
 	for numGuesses < maxNumGuesses {
 		numGuesses++
-		nextGuess := getOptimalNextGuess(remainingWordList)
-		colourPattern := getColourPattern(nextGuess, answer)
-		if reflect.DeepEqual(colourPattern, correctGuessColourPattern) {
+		guess := getOptimalNextGuess(remainingWordList)
+		guessColourPattern := getColourPattern(guess, answer)
+		if reflect.DeepEqual(guessColourPattern, correctGuessColourPattern) {
 			gameWon = true
 			break
 		}
-		// TODO - update remainingWordList to only include words compatible with colourPattern
+		// is this dumb?
+		remainingWordList = filterWordListBasedOnLatestGuess(remainingWordList, guessColourPattern, answer)
+		log.Printf("len(remainingWordList)=%d", len(remainingWordList))
 	}
 
 	if gameWon {
@@ -114,6 +148,8 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	log.Println(getOptimalNextGuess(wordList))
 
 	answer := "hello"
 	log.Printf("(The answer is: %q - shhhhhh!)", answer)
