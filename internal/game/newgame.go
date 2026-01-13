57628179019f9bcf4d058902abc1dbb746794f91
hnasser-dev <hnasser.dev@proton.me>
2026-01-13 18:50:02 +0000

    further progress on newgame.go

--- internal/game/newgame.go
+++ internal/game/newgame.go
@@ -1,16 +1,23 @@
 package game
 
-import "github.com/hnasser-dev/wordle-solver/internal/words"
+import (
+	"fmt"
+	"reflect"
+
+	"github.com/hnasser-dev/wordle-solver/internal/words"
+)
 
 type Game struct {
+	Answer                  string
 	GameWon                 bool
 	Guesses                 []string
 	InitialWordList         []string
 	RemainingWordList       []string
 	SortedRemainingOutcomes []guessOutcome
+	WordFrequencies         words.WordFrequencyMap
 }
 
-func NewGame() (*Game, error) {
+func NewGame(answer string) (*Game, error) {
 
 	initialWordList, err := words.GetWordList()
 	if err != nil {
@@ -19,7 +26,20 @@ func NewGame() (*Game, error) {
 	remainingWordList := make([]string, len(initialWordList))
 	copy(remainingWordList, initialWordList)
 
+	answerInWordList := false
+	for _, word := range initialWordList {
+		if word == answer {
+			answerInWordList = true
+			break
+		}
+	}
+
+	if !answerInWordList {
+		return nil, fmt.Errorf("provided answer %q is not in the word list", answer)
+	}
+
 	game := Game{
+		Answer:                  answer,
 		GameWon:                 false,
 		Guesses:                 []string{},
 		InitialWordList:         initialWordList,
@@ -30,8 +50,16 @@ func NewGame() (*Game, error) {
 	return &game, nil
 }
 
-func (g *Game) Guess(guess string) {
-	// do a guess
+// Returns: gameWon (bool)
+func (g *Game) Guess(guess string) bool {
+	g.SortedRemainingOutcomes = getSortedGuessOutcomes(g.RemainingWordList, g.WordFrequencies)
+	bestOutcome := g.SortedRemainingOutcomes[0]
+	g.Guesses = append(g.Guesses, bestOutcome.guess)
+	nextColourPattern := getColourPattern(bestOutcome.guess, g.Answer)
+	if reflect.DeepEqual(nextColourPattern, correctGuessColourPattern) {
+		g.GameWon = true
+	}
+	return g.GameWon
 }
 
 func (g *Game) PlayGameUntilEnd(limitGuesses bool) {
