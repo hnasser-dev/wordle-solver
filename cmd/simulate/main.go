c05c226d5942bf8cb29aedf615ac9130138990f6
hnasser-dev <hnasser.dev@proton.me>
2026-01-17 23:29:44 +0000

    simulating each starting guess (currently got a limit) and logging their avgGuesses and winRates - need to think about race conditions

--- cmd/simulate/main.go
+++ cmd/simulate/main.go
@@ -5,11 +5,22 @@ import (
 	"log/slog"
 	"os"
 	"runtime"
+	"sort"
 	"sync"
 
 	"github.com/hnasser-dev/wordle-solver/internal/game"
 	"github.com/hnasser-dev/wordle-solver/internal/words"
 )
+
+type avgGuessesEntry struct {
+	word       string
+	avgGuesses float64
+}
+
+type winRatesEntry struct {
+	word    string
+	winRate float64
+}
 
 // Simulate a game for each possible answer
 func main() {
@@ -28,33 +39,85 @@ func main() {
 		os.Exit(1)
 	}
 
-	freqMap, err := words.GetWordFrequencyMap()
-	if err != nil {
-		slog.Error("unable to read frequency map", "err", err)
-		os.Exit(1)
-	}
+	// independent subset of list
+	startingGuessesList := append([]string{}, wordList[:8]...)
 
-	// simulate a game for all possible answers - multiprocess and limit concurrency
+	allAvgGuesses := make([]avgGuessesEntry, 0, len(startingGuessesList))
+	allWinRates := make([]winRatesEntry, 0, len(startingGuessesList))
+
+	// simulate a game for all possible startingGuesses and answers
 	var wg sync.WaitGroup
 	sem := make(chan struct{}, runtime.NumCPU())
 
-	for idx, answer := range wordList {
-		wg.Add(1)
-		sem <- struct{}{} // block until there is space in the sem
-		go func() {
-			defer wg.Done()
-			defer func() { <-sem }()
-			guesses, gameWon := game.PlayGame(answer, wordList, freqMap, nil)
-			fileLogger.Info(
-				"game complete",
-				slog.Int("gameNum", idx+1),
-				slog.String("answer", answer),
-				slog.Int("numGuesses", len(guesses)),
-				slog.Any("guesses", guesses),
-				slog.Bool("gameWon", gameWon),
-			)
-		}()
+	slog.Info("starting simulation", slog.Int("numStartingGuesses", len(startingGuessesList)))
+	for _, startingGuess := range startingGuessesList {
+		totalNumGames := 0
+		totalNumGuesses := 0
+		totalNumWins := 0
+		for _, answer := range wordList {
+			wg.Add(1)
+			sem <- struct{}{} // block until there is space in the sem
+			go func() {
+				defer wg.Done()
+				defer func() { <-sem }()
+				initialGuesses := []string{startingGuess}
+				game, err := game.NewGame(game.GameConfig{
+					Answer:         answer,
+					GameMode:       game.NormalMode,
+					InitialGuesses: initialGuesses,
+					WordList:       wordList,
+				})
+				if err != nil {
+					slog.Error("unable to create new game", "err", err)
+					os.Exit(1)
+				}
+				gameWon, guesses := game.PlayGameUntilEnd(false)
+				totalNumGames++
+				totalNumGuesses += len(guesses)
+				if gameWon {
+					totalNumWins++
+				}
+			}()
+		}
+		avgGuesses := float64(totalNumGuesses) / float64(totalNumWins)
+		allAvgGuesses = append(
+			allAvgGuesses,
+			avgGuessesEntry{
+				word:       startingGuess,
+				avgGuesses: avgGuesses,
+			},
+		)
+		winRate := float64(totalNumWins) / float64(totalNumGames)
+		allWinRates = append(
+			allWinRates,
+			winRatesEntry{
+				word:    startingGuess,
+				winRate: winRate,
+			},
+		)
+		fileLogger.Info(
+			"iteration complete",
+			slog.String("startingGuess", startingGuess),
+			slog.Float64("avgGuess", avgGuesses),
+			slog.Float64("winRate", winRate),
+		)
 	}
+	slog.Info("completed simulation", slog.Int("numStartingGuesses", len(startingGuessesList)))
+
+	// sorted allAvgGuesses, high to low
+	sort.Slice(
+		allAvgGuesses,
+		func(i, j int) bool { return allAvgGuesses[i].avgGuesses < allAvgGuesses[j].avgGuesses },
+	)
+
+	// sorted allWinRates, high to low
+	sort.Slice(
+		allWinRates,
+		func(i, j int) bool { return allWinRates[i].winRate > allWinRates[j].winRate },
+	)
+
+	slog.Info("best words by avgGuesses", "wordsAndAvgGuesses", allAvgGuesses[:5])
+	slog.Info("best words by winRate", "wordsAndWinRates", allWinRates[:5])
 
 	wg.Wait()
 }
